490c40c61da065130274882539a671b631c14c5d
Cheranga Hatangala <cheranga@gmail.com>
2019-02-28 21:18:03 +1100

    Add project files.

---
+++ DbUpConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using DbUp;
+
+namespace DbUpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Updating Database. Please stay tuned...");
+
+            InitDatabase();
+        }
+
+        private static int InitDatabase()
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BlogDb;Integrated Security=True;Connect Timeout=30;";
+            
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var dbUpgradeEngineBuilder = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(typeof(Program).Assembly)
+                .WithTransaction()
+                .LogToConsole();
+
+            var dbUpgradeEngine = dbUpgradeEngineBuilder.Build();
+            if (dbUpgradeEngine.IsUpgradeRequired())
+            {
+                Console.WriteLine("Upgrades have been detecred. Upgrading database now...");
+                var operation = dbUpgradeEngine.PerformUpgrade();
+                if (operation.Successful)
+                {
+                    Console.WriteLine("Upgrade completed successfully");
+                    Console.ReadLine();
+
+                    return 0;
+                }
+
+                Console.WriteLine("Error happened in the upgrade. Please check the logs");
+                Console.ReadLine();
+
+                return -1;
+            }
+
+            return 0;
+
+        }
+    }
+}
---
+++ DbUpConsole/Scripts/001 - Create tblPosts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblPosts]
+(
+	[PostId] INT NOT NULL PRIMARY KEY, 
+    [Id] INT NOT NULL, 
+    [UserId] INT NOT NULL,
+    [Title] NVARCHAR(200) NOT NULL, 
+    [Body] NVARCHAR(500) NOT NULL, 
+)
